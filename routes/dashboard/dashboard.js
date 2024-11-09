003e7d36afe9dd1849d1307a20b5a8fc0d4014e1
Biniyam kefyalew <165114010+right-hand-boy@users.noreply.github.com>
2024-11-09 16:17:59 +0300

    Update dashboard.js

--- routes/dashboard/dashboard.js
+++ routes/dashboard/dashboard.js
@@ -5,16 +5,12 @@ const router = express.Router();
 const { reportData } = require("./reportData");
 const { barData } = require("./barData");
 const { lineData } = require("./lineData");
-const { visitorData } = require("./visitor");
-const { visitorCityData } = require("./visitorcity");
 const { topSellingProducts } = require("./topSellingProducts");
 
 // Export the function
 router.get("/report-data", reportData);
 router.get("/bar-data", barData);
 router.get("/line-data", lineData);
-router.get("/visitor-data", visitorData);
-router.get("/visitor-city-data", visitorCityData);
 router.get("/top-selling-products", topSellingProducts);
 
 module.exports = router;
